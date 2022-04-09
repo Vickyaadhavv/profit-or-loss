ba09d9ad18b1034e6e865588e8903e4edd0ebe07
Vickyaadhavv <102547223+Vickyaadhavv@users.noreply.github.com>
2022-04-09 15:06:05 +0530

    p or l

---
+++ app.js
@@ -0,0 +1,37 @@
+var initialprice= document.querySelector("#initial-price");
+var stockquantity= document.querySelector("#stocks-quantity");
+var currentprice= document.querySelector("#current-price");
+var submitbtn= document.querySelector("#submit-btn");
+var outputbox= document.querySelector("#output-box"); 
+
+
+submitbtn.addEventListener("click",submitHandler)
+function submitHandler() {
+    var ip=Number(initialprice.value);   
+    var qty=Number(stockquantity.value);
+    var curr= Number(currentprice.value);
+
+    calulateprofitandloss(ip,qty,curr)
+}
+
+
+function calulateprofitandloss(initial,quantity,current,) { 
+ if(initial>current){
+    var loss =(initial-current)*quantity;
+    var losspercentage =(loss/initial)*100;
+    showoutputbox(`Hey the loss is ${loss} and the percentage is ${losspercentage}%`);
+ }else if (current>initial){
+    var profit =(current-initial)*quantity;
+    var profitpercentage =(profit/initial)*100;
+   showoutputbox(`Hey the profit is ${profit} and the percentage is ${profitpercentage}%`);
+
+ }else {
+  showoutputbox(`No pain No gain`);
+ }
+
+};
+
+function showoutputbox(message) {
+    outputbox.innerHTML=message
+    
+}
